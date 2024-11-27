4a26f4f44b99b1074db0e199de383af8e91d7663
KolyaNaumenko <113513424+KolyaNaumenko@users.noreply.github.com>
2024-11-27 10:21:09 +0200

    ci +req 3.10

--- myproject/products/tests.py
+++ myproject/products/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+import pytest
+from myproject  import manage  # Імпортуємо WSGI для тестування Django
 
+@pytest.fixture
+def client():
+    from django.test.client import Client
+    return Client()
+
+def test_products_endpoint(client):
+    response = client.get('/products/1')
+    assert response.status_code == 200
+    assert response.json() == {"id": "1", "name": "1 name"}
 # Create your tests here.
