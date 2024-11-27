44e731b3de5c304d7b9e152d9f9cd3728cc836af
KolyaNaumenko <113513424+KolyaNaumenko@users.noreply.github.com>
2024-11-27 09:41:13 +0200

    tak2

---
+++ test/test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from myproject  import manage  # Імпортуємо WSGI для тестування Django
+
+@pytest.fixture
+def client():
+    from django.test.client import Client
+    return Client()
+
+def test_products_endpoint(client):
+    response = client.get('/products/1')
+    assert response.status_code == 200
+    assert response.json() == {"id": "1", "name": "1 name"}
